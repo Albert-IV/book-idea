4c939d2c294b7e6077fdfbd9925ca8f64d4d3ab7
Albert Engelbrecht <me@albertengelbrecht.com>
2019-09-25 08:07:05 -0500

    Fixed compilation, got toggle working! 🎉

--- site/src/Main.elm
+++ site/src/Main.elm
@@ -7,9 +7,11 @@
 
 module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
 
+import Array exposing (..)
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 import Http
 import List
 import Models exposing (..)
@@ -34,6 +36,7 @@ main =
 
 type Model
     = DisplayBook Book
+    | ErrorUpdatingBook Book String
 
 
 init : () -> ( Model, Cmd Msg )
@@ -45,13 +48,115 @@ init _ =
 
 type Msg
     = SelectedBook Book
+    | ToggleMastery ClickMsg
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        SelectedBook book ->
-            ( DisplayBook book, Cmd.none )
+    case model of
+        DisplayBook currentBook ->
+            case msg of
+                SelectedBook book ->
+                    Debug.log "Loaded book" ( DisplayBook book, Cmd.none )
+
+                ToggleMastery clickEvent ->
+                    case clickEvent.msgType of
+                        PartRecord ->
+                            Debug.log "this should be updating the book..." ( togglePartMastery clickEvent currentBook, Cmd.none )
+
+                        ChapterRecord ->
+                            Debug.log "Chapter updated?" ( DisplayBook (toggleChapterMastery clickEvent currentBook), Cmd.none )
+
+        _ ->
+            Debug.log "Nothing... happened?" ( model, Cmd.none )
+
+
+getPart : Int -> Book -> Maybe Part
+getPart partIdx book =
+    get partIdx book.parts
+
+
+getChapter : Int -> Int -> Book -> Maybe Chapter
+getChapter chapterIdx partIdx book =
+    case getPart partIdx book of
+        Nothing ->
+            Nothing
+
+        Just part ->
+            get chapterIdx part.chapters
+
+
+toggleRead : Mastery -> Mastery
+toggleRead mastery =
+    { mastery | read = not mastery.read }
+
+
+toggleExample : Mastery -> Mastery
+toggleExample mastery =
+    { mastery | examples = not mastery.examples }
+
+
+toggleResearch : Mastery -> Mastery
+toggleResearch mastery =
+    { mastery | moreResearch = not mastery.moreResearch }
+
+
+togglePractice : Mastery -> Mastery
+togglePractice mastery =
+    { mastery | morePractice = not mastery.morePractice }
+
+
+togglePartMastery : ClickMsg -> Book -> Model
+togglePartMastery msg book =
+    let
+        maybePart =
+            getPart msg.partIdx book
+
+        updatedMastery =
+            case maybePart of
+                Nothing ->
+                    Debug.log "Found nothing for maybePart" Nothing
+
+                Just part ->
+                    case part.mastery of
+                        Nothing ->
+                            Debug.log "Found nothing for maybePart.mastery" Nothing
+
+                        Just mastery ->
+                            Debug.log "Toggling mastery" Just (toggleRead mastery)
+
+        updatedPart =
+            case maybePart of
+                Nothing ->
+                    Debug.log "Found nothing for maybePart" Nothing
+
+                Just part ->
+                    case updatedMastery of
+                        Nothing ->
+                            Debug.log "Found nothing for maybePart.mastery" Nothing
+
+                        Just mastery ->
+                            Debug.log "Setting new mastery for part" Just { part | mastery = Just mastery }
+
+        updatedBook =
+            case updatedPart of
+                Nothing ->
+                    Debug.log "Found nothing for updatedPart" Nothing
+
+                Just newPart ->
+                    Debug.log "Setting new part on book object" Just { book | parts = set msg.partIdx newPart book.parts }
+    in
+    case updatedBook of
+        Nothing ->
+            Debug.log "error updating book" ErrorUpdatingBook book "Unable to update book!"
+
+        Just newBook ->
+            Debug.log "this should be getting the new book :thinking_face:" DisplayBook newBook
+
+
+toggleChapterMastery : ClickMsg -> Book -> Book
+toggleChapterMastery msg book =
+    book
 
 
 
@@ -95,31 +200,59 @@ view model =
                 [ h1 []
                     [ text book.title ]
                 , div []
-                    [ div [] (List.map partContent book.parts)
+                    [ div [] (toList (indexedMap partContent book.parts))
+                    ]
+                ]
+
+        ErrorUpdatingBook book msg ->
+            div [ class "container" ]
+                [ h1 [] [ text msg ]
+                , h1 []
+                    [ text book.title ]
+                , div []
+                    [ div [] (toList (indexedMap partContent book.parts))
                     ]
                 ]
 
 
-partContent : Part -> Html Msg
-partContent part =
+partContent : Int -> Part -> Html Msg
+partContent partIdx part =
+    let
+        chapterTuples =
+            Array.map (\chapter -> ( partIdx, chapter )) part.chapters
+    in
     div [ class "card mb-3" ]
-        [ div [ class "card-header d-flex justify-content-between align-items-center" ] (buttonContent part)
-        , ul [ class "list-group list-group-flush" ] (List.map chapterContent part.chapters)
+        [ div
+            [ class "card-header d-flex justify-content-between align-items-center" ]
+            (buttonContent part partIdx 0 PartRecord)
+        , ul [ class "list-group list-group-flush" ] (toList (indexedMap chapterContent chapterTuples))
         ]
 
 
-chapterContent : Chapter -> Html Msg
-chapterContent chapter =
-    case chapter.mastery of
-        Nothing ->
-            li [ class "list-group-item" ] [ text chapter.name ]
+chapterContent : Int -> ( Int, Chapter ) -> Html Msg
+chapterContent partIdx chapterTuple =
+    let
+        chapterIdx =
+            Tuple.first chapterTuple
 
-        Just mastery ->
-            li [ class "list-group-item d-flex justify-content-between align-items-center" ] (buttonContent chapter)
+        chapter =
+            Tuple.second chapterTuple
+
+        contents =
+            case chapter.mastery of
+                Nothing ->
+                    li [ class "list-group-item" ] [ text chapter.name ]
+
+                Just mastery ->
+                    li
+                        [ class "list-group-item d-flex justify-content-between align-items-center" ]
+                        (buttonContent chapter partIdx chapterIdx ChapterRecord)
+    in
+    contents
 
 
-buttonContent : BookButtons a -> List (Html Msg)
-buttonContent chapterOrPart =
+buttonContent : BookButtons a -> Int -> Int -> RecordType -> List (Html Msg)
+buttonContent chapterOrPart partIdx chapterIdx msgType =
     case chapterOrPart.mastery of
         Nothing ->
             [ text chapterOrPart.name ]
@@ -127,22 +260,23 @@ buttonContent chapterOrPart =
         Just mastery ->
             text chapterOrPart.name
                 :: div []
-                    [ masteryReadBtn mastery
-                    , masteryExamplesBtn mastery
-                    , masteryResearchBtn mastery
-                    , masteryPracticeBtn mastery
+                    [ masteryReadBtn mastery (ClickMsg partIdx chapterIdx "read" msgType)
+                    , masteryExamplesBtn mastery (ClickMsg partIdx chapterIdx "examples" msgType)
+                    , masteryResearchBtn mastery (ClickMsg partIdx chapterIdx "moreResearch" msgType)
+                    , masteryPracticeBtn mastery (ClickMsg partIdx chapterIdx "morePractice" msgType)
                     ]
                 :: []
 
 
-masteryReadBtn : Mastery -> Html Msg
-masteryReadBtn mastery =
+masteryReadBtn : Mastery -> ClickMsg -> Html Msg
+masteryReadBtn mastery clickMsg =
     button
         [ classList
             [ ( "card-link btn", True )
             , ( "btn-primary", mastery.read )
             , ( "btn-danger", not mastery.read )
             ]
+        , onClick (ToggleMastery clickMsg)
         ]
         [ text (masteryReadText mastery) ]
 
@@ -156,14 +290,15 @@ masteryReadText mastery =
         "Chapter Unread"
 
 
-masteryExamplesBtn : Mastery -> Html Msg
-masteryExamplesBtn mastery =
+masteryExamplesBtn : Mastery -> ClickMsg -> Html Msg
+masteryExamplesBtn mastery clickMsg =
     button
         [ classList
             [ ( "card-link btn", True )
             , ( "btn-primary", mastery.examples )
             , ( "btn-danger", not mastery.examples )
             ]
+        , onClick (ToggleMastery clickMsg)
         ]
         [ text (masteryExamplesText mastery) ]
 
@@ -177,14 +312,15 @@ masteryExamplesText mastery =
         "Examples Incomplete"
 
 
-masteryResearchBtn : Mastery -> Html Msg
-masteryResearchBtn mastery =
+masteryResearchBtn : Mastery -> ClickMsg -> Html Msg
+masteryResearchBtn mastery clickMsg =
     button
         [ classList
             [ ( "card-link btn", True )
             , ( "btn-primary", mastery.moreResearch )
             , ( "btn-danger", not mastery.moreResearch )
             ]
+        , onClick (ToggleMastery clickMsg)
         ]
         [ text (masteryResearchText mastery) ]
 
@@ -198,14 +334,15 @@ masteryResearchText mastery =
         "No Research Necessary"
 
 
-masteryPracticeBtn : Mastery -> Html Msg
-masteryPracticeBtn mastery =
+masteryPracticeBtn : Mastery -> ClickMsg -> Html Msg
+masteryPracticeBtn mastery clickMsg =
     button
         [ classList
             [ ( "card-link btn", True )
             , ( "btn-primary", mastery.morePractice )
             , ( "btn-danger", not mastery.morePractice )
             ]
+        , onClick (ToggleMastery clickMsg)
         ]
         [ text (masteryPracticeText mastery) ]
 
--- site/src/Models.elm
+++ site/src/Models.elm
@@ -1,18 +1,20 @@
-module Models exposing (Book, BookButtons, Chapter, Mastery, Part, initialBook)
+module Models exposing (Book, BookButtons, Chapter, ClickMsg, Mastery, Part, RecordType(..), emptyMastery, initialBook)
+
+import Array exposing (..)
 
 
 type alias Book =
     { title : String
     , author : String
     , isbn : String
-    , parts : List Part
+    , parts : Array Part
     }
 
 
 type alias Part =
     { name : String
     , mastery : Maybe Mastery
-    , chapters : List Chapter
+    , chapters : Array Chapter
     }
 
 
@@ -34,60 +36,84 @@ type alias Mastery =
     }
 
 
+type alias ClickMsg =
+    { partIdx : Int
+    , chapterIdx : Int
+    , mastery : String
+    , msgType : RecordType
+    }
+
+
+type RecordType
+    = PartRecord
+    | ChapterRecord
+
+
 initialBook : Book
 initialBook =
     Book
         "Cracking the Coding Interview"
         "AYY"
         "9010"
-        [ Part "Big O"
-            Nothing
-            [ Chapter "Big-O Notation" (emptyMastery True)
+        (Array.fromList
+            [ Part "Big O"
+                (emptyMastery True)
+                (Array.fromList [ Chapter "Big-O Notation" Nothing ])
+            , Part "Data Structures"
+                Nothing
+                (Array.fromList
+                    [ Chapter "Arrays and Strings" (emptyMastery True)
+                    , Chapter "Linked Lists" (emptyMastery True)
+                    , Chapter "Stacks and Queues" (emptyMastery True)
+                    , Chapter "Trees and Graphs" (emptyMastery True)
+                    ]
+                )
+            , Part "Concepts and Algorithims"
+                Nothing
+                (Array.fromList
+                    [ Chapter "Bit Manipulation" (emptyMastery True)
+                    , Chapter "Math & Logic Puzzles" (emptyMastery True)
+                    , Chapter "Object Oriented Design" (emptyMastery True)
+                    , Chapter "Recursion and Dynamic Programming" Nothing
+                    , Chapter "System Design and Scalability" Nothing
+                    , Chapter "Sorting and Searching" Nothing
+                    , Chapter "Testing" Nothing
+                    ]
+                )
+            , Part "Knowledge Based"
+                Nothing
+                (Array.fromList
+                    [ Chapter "C and C++" Nothing
+                    , Chapter "Java" Nothing
+                    , Chapter "Databases" Nothing
+                    , Chapter "Threads and Locks" Nothing
+                    ]
+                )
+            , Part "Advanced Topics"
+                (emptyMastery True)
+                (Array.fromList
+                    [ Chapter "Useful Math" Nothing
+                    , Chapter "Topological Sort" Nothing
+                    , Chapter "Dijkstra's Algorithm" Nothing
+                    , Chapter "Hash Table Collision Resolution" Nothing
+                    , Chapter "Rabin-Karp Substring Search" Nothing
+                    , Chapter "AVL Trees" Nothing
+                    , Chapter "Red-Black Trees" Nothing
+                    , Chapter "MapReduce" Nothing
+                    , Chapter "Additional Studying" Nothing
+                    ]
+                )
+            , Part "Code Library"
+                (emptyMastery False)
+                (Array.fromList
+                    [ Chapter "HashMapList<T,E>" Nothing
+                    , Chapter "TreeNode (Binary Search Tree)" Nothing
+                    , Chapter "LinkedListNode (Linked List)" Nothing
+                    , Chapter "Trie & TrieNode" Nothing
+                    ]
+                )
             ]
-        , Part "Data Structures"
-            Nothing
-            [ Chapter "Arrays and Strings" (emptyMastery True)
-            , Chapter "Linked Lists" (emptyMastery True)
-            , Chapter "Stacks and Queues" (emptyMastery True)
-            , Chapter "Trees and Graphs" (emptyMastery True)
-            ]
-        , Part "Concepts and Algorithims"
-            Nothing
-            [ Chapter "Bit Manipulation" (emptyMastery True)
-            , Chapter "Math & Logic Puzzles" (emptyMastery True)
-            , Chapter "Object Oriented Design" (emptyMastery True)
-            , Chapter "Recursion and Dynamic Programming" Nothing
-            , Chapter "System Design and Scalability" Nothing
-            , Chapter "Sorting and Searching" Nothing
-            , Chapter "Testing" Nothing
-            ]
-        , Part "Knowledge Based"
-            Nothing
-            [ Chapter "C and C++" Nothing
-            , Chapter "Java" Nothing
-            , Chapter "Databases" Nothing
-            , Chapter "Threads and Locks" Nothing
-            ]
-        , Part "Advanced Topics"
-            (emptyMastery True)
-            [ Chapter "Useful Math" Nothing
-            , Chapter "Topological Sort" Nothing
-            , Chapter "Dijkstra's Algorithm" Nothing
-            , Chapter "Hash Table Collision Resolution" Nothing
-            , Chapter "Rabin-Karp Substring Search" Nothing
-            , Chapter "AVL Trees" Nothing
-            , Chapter "Red-Black Trees" Nothing
-            , Chapter "MapReduce" Nothing
-            , Chapter "Additional Studying" Nothing
-            ]
-        , Part "Code Library"
-            (emptyMastery False)
-            [ Chapter "HashMapList<T,E>" Nothing
-            , Chapter "TreeNode (Binary Search Tree)" Nothing
-            , Chapter "LinkedListNode (Linked List)" Nothing
-            , Chapter "Trie & TrieNode" Nothing
-            ]
-        ]
+        )
 
 
 emptyMastery : Bool -> Maybe Mastery
