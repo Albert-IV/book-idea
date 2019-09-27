3a53e46bf123a02359a9e9351b3b85d553d92355
Albert Engelbrecht <me@albertengelbrecht.com>
2019-09-26 20:59:46 -0500

    Removed boilerplate comments.

--- site/src/Main.elm
+++ site/src/Main.elm
@@ -1,10 +1,3 @@
--- Make a GET request to load a book called "Public Opinion"
---
--- Read how it works:
---   https://guide.elm-lang.org/effects/http.html
---
-
-
 module Main exposing (Model(..), Msg(..), init, main, subscriptions, update, view)
 
 import Array exposing (..)
@@ -83,7 +76,7 @@ getChapter chapterIdx partIdx book =
             Nothing
 
         Just part ->
-            get chapterIdx part.chapters
+            Array.get chapterIdx part.chapters
 
 
 toggleRead : Mastery -> Mastery
